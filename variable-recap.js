ff57d9a3ff0146d18637996e045885bfb51e2afa
MirzaAyon <ayonmirza98@gmail.com>
2022-05-11 10:44:33 +0600

    variable recap added

---
+++ variable-recap.js
@@ -0,0 +1,14 @@
+//variable 
+//number
+var watchPrice = 120;
+watchPrice = 125;
+
+//String
+
+var userName = 'Sogir Uddin';
+
+//Boolean
+var isExpensive = true;
+
+//int 12,23,34,,45
+//float 2.34, 4.54
