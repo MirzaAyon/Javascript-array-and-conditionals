79f6c3b984075c3fdb41973e9aaea0d70c667f4b
MirzaAyon <ayonmirza98@gmail.com>
2022-05-12 16:36:54 +0600

    array added

---
+++ array.js
@@ -0,0 +1,17 @@
+var friendAge = 11; //ekta bostu rakhte hole
+var friendAge2 = [20, 23, 45]; //ekadhik jinish rakhte hole
+
+var treatFee = [12, 45, 74, 23];
+
+var favNayika = 'mahi'; //single element
+
+var favNayika2 = ['mahi', 'fariha', 'avril', 'alia']; //double element
+
+var oddNumbers = [1, 3, 5, 7, 9];//array number
+
+var vowels = ['a', 'e', 'i', 'o', 'u']; //array string
+
+console.log(friendAge2);
+console.log(vowels);
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
