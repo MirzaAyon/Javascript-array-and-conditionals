f10385af31afee24a1f4157e9dd6f370fd69cfab
MirzaAyon <ayonmirza98@gmail.com>
2022-05-12 17:26:25 +0600

    push-pop added

---
+++ push-pop.js
@@ -0,0 +1,34 @@
+var lastBench = ['kalam', 'balam', 'salam'];
+console.log(lastBench);
+lastBench.push('jalam');
+console.log(lastBench);
+lastBench.push('palam');
+console.log(lastBench);
+lastBench.push('nalam');
+console.log(lastBench);
+//string push korechi
+
+var friendsAge = [12, 23, 34, 56];
+console.log(friendsAge);
+friendsAge.push(89);
+console.log(friendsAge);
+//number push korechi
+
+var Age = [12, 23, 34, 56];
+console.log(Age);
+Age.push(89);
+console.log(Age);
+Age.pop();
+console.log(Age);
+Age.pop();
+console.log(Age);
+//number pop korechi
+//pop er khetre bracket e kichu likhte hoyna shobar last er element nijei pop hoye jae
+//last e shesh e pop korte korte r na thakle [] eta show korbe 
+
+var lastItem = Age.pop();
+console.log(Age);
+console.log(lastItem);
+//pop korechi and last je item ke pop korechi ta ber korechi
+
+//khatae 2 ta task likha ache 
