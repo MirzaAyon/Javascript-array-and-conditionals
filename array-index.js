05159168e00c37fba0a7a5193569c94bb83dba79
MirzaAyon <ayonmirza98@gmail.com>
2022-05-12 17:09:33 +0600

    array index added

---
+++ array-index.js
@@ -0,0 +1,18 @@
+var mySelf = ['ayon', 'plabon', 'dui', 'bhai', 'chino', 'oderke'];
+
+var bhaiIndex = mySelf.indexOf('bhai');
+console.log(bhaiIndex);
+//index mane shuchi/obosthan/posithon
+//bhai er position bolse
+var secondIndex = mySelf[2];
+console.log(secondIndex)
+//2nd index e ke ache ta bolse
+var secondIndex = mySelf[112];
+console.log(secondIndex)
+//undefined ashbe karon er kono positiion nai 
+
+
+var numbers = [12, 23, 34, 45, 56, 78, 89];
+// var index = numbers.indexOf(45);
+var index = numbers.indexOf(145); //array er modhe nai tai position -1
+console.log(index);
