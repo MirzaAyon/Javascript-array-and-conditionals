f9893fae863e8b63cf85f58af6221d7e33b73efa
MirzaAyon <ayonmirza98@gmail.com>
2022-05-12 22:35:20 +0600

    if else condition added

---
+++ conditional.js
@@ -0,0 +1,36 @@
+var isFoodReady = true;
+
+if (isFoodReady == true) {
+    console.log('Mama! I will eat now');
+}
+
+// var phonePrice = 70000;
+// var budget = 9500;
+
+// if (phonePrice > budget) {
+//     console.log('Kinbona');
+// }
+
+var phonePrice = 700;
+var budget = 9500;
+
+if (phonePrice < budget) {
+    console.log('Kinbo');
+}
+//duita alada condition tai ekta dekhale arekta dekhae na
+
+// if (condition) {
+//     //
+// } else {
+
+// }
+//structure
+
+var chickenPrice = 180;
+var myMoney = 50;
+
+if (chickenPrice < myMoney) {
+    console.log('Khabo');
+} else {
+    console.log('Khabo na');
+}
