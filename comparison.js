885df03b817b549e9e8b7c2a90bfd6fcfac4b322
MirzaAyon <ayonmirza98@gmail.com>
2022-05-12 20:10:29 +0600

    comparison variables and operator added

---
+++ comparison.js
@@ -0,0 +1,17 @@
+console.log(5 > 6);
+console.log(5 < 6);
+console.log(5 == 6);
+console.log(6 == 6);
+console.log(6 != 6);
+var myLove = 99;
+var yourLove = 100;
+console.log(myLove > yourLove)
+console.log(myLove < yourLove)
+console.log(myLove == yourLove)
+console.log(myLove = 100 == yourLove)
+console.log(myLove != yourLove)
+
+// && || 
+
+console.log(Good && handsome); //2 tai lagbe 
+console.log(Good || handsome); // jekono ekta korlei hbe 
