a6d86d365e6f134731b7ffb28cb774260096fa78
MirzaAyon <ayonmirza98@gmail.com>
2022-05-15 12:41:19 +0600

    nested condition added

---
+++ nested-condition.js
@@ -0,0 +1,18 @@
+var danishPrice = 50;
+var butterBread = 25;
+var toastBiscuitPrice = 10;
+var myBudget = 100;
+
+if (danishPrice < myBudget) {
+    console.log('Danish Khabo');
+}
+else if (butterBread < myBudget) {
+    console.log('Butter Bon Khabo');
+}
+else if (toastBiscuitPrice < myBudget) {
+    console.log('toast khabo');
+}
+else {
+    console.log('Batasha Khabo');
+}
+//kaj ta same gpa grade er moto 
---
+++ nested-condition2.js
@@ -0,0 +1,14 @@
+var danishPrice = 50;
+var butterBread = 25;
+var toastBiscuitPrice = 10;
+var myBudget = 100;
+var packedWell = false;
+
+if (danishPrice < myBudget) {
+    if (packedWell == true) {
+        console.log('danish khabo');
+    }
+    else {
+        console.log('khabo na');
+    }
+}
