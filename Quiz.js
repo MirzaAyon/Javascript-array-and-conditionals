dee9de077038c2a03442629eba0f18ae2c36424f
MirzaAyon <ayonmirza98@gmail.com>
2022-07-03 12:40:00 +0600

    some questions added

---
+++ Quiz.js
@@ -0,0 +1,10 @@
+//3 - How will you get the second element of the array arr = [3, 7, 10]?
+//arr.indexOf(1)
+
+//6 - What would nums.indexOf(0) and nums.indexOf(1) return respectively, if var nums = [1, 5, 8]?
+// ans - -1 and 0
+
+//11 - How to replace the second element in the array, arr = [1, 7, 9] with the value 8?
+// ans : arr[1] = 8;
+
+//push/ pop/shift/unshift shikhbo
